33f662a6d7740dfb3afbdb3b9c62c5e81dac035f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-09-26 21:14:40 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/casestudy/migroscouriertracking/MigroscouriertrackingApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.migroscouriertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigroscouriertrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigroscouriertrackingApplication.class, args);
+	}
+
+}
