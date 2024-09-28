1f122dd3c74b78b55a41b6658f93dd604ee83f04
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-09-28 14:44:00 +0300

    Task 18 : Define DistanceCalculationUtilTest

---
+++ src/test/java/com/casestudy/migroscouriertracking/courier/utils/DistanceCalculationUtilTest.java
@@ -0,0 +1,33 @@
+package com.casestudy.migroscouriertracking.courier.utils;
+
+import com.casestudy.migroscouriertracking.courier.model.Location;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistanceCalculationUtilTest {
+
+    private Location startLoc;
+    private Location endLoc;
+
+    @BeforeEach
+    void setUp() {
+        // Initialize some sample locations for testing
+        startLoc = new Location(39.9255, 32.8662); // Ankara
+        endLoc = new Location(39.9255, 32.8662); // Ankara
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionForInvalidDistanceType() {
+
+        // Given & When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            DistanceCalculationUtil.calculateDistance(startLoc, endLoc, null);
+        });
+
+        // Then
+        assertEquals("Invalid distance type", exception.getMessage());
+
+    }
+}
